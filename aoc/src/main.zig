cfc2fee51e03aecfbfac3c9dc8953204707df80a
Lucien Faure <faurelucien@proton.me>
2026-02-09 22:18:36 +0100

    learning zig

---
+++ aoc/src/main.zig
@@ -0,0 +1,52 @@
+const std = @import("std");
+
+const aoc = @import("aoc");
+
+fn print_slice(slice: []const u8) void {
+    if (slice.len == 0) return;
+    std.debug.print("|", .{});
+    for (slice) |elem| {
+        std.debug.print("{d}|", .{elem});
+    }
+    std.debug.print("\n", .{});
+}
+
+pub fn main(init: std.process.Init) !void {
+    const gpa = init.gpa;
+    const args: []const []const u8 = try init.minimal.args.toSlice(gpa);
+    defer gpa.free(args);
+
+    for (1..args.len) |i| {
+        std.debug.print("{s}\n", .{args[i]});
+    }
+    var random: [10]u8 = undefined;
+    for (&random, 0..) |*elem, i| {
+        elem.* = @intCast(i);
+    }
+    var prng: std.Random.DefaultPrng = .init(51235123);
+    const rand = prng.random();
+    std.Random.shuffle(rand, u8, &random);
+    std.debug.print("random:\n", .{});
+    print_slice(&random);
+    std.debug.print("\n", .{});
+}
+
+// test "simple test" {
+//     const gpa = std.testing.allocator;
+//     var list: std.ArrayList(i32) = .empty;
+
+//     defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
+//     try list.append(gpa, 42);
+//     try std.testing.expectEqual(@as(i32, 42), list.pop());
+// }
+
+// test "fuzz example" {
+//     const Context = struct {
+//         fn testOne(context: @This(), input: []const u8) anyerror!void {
+//             _ = context;
+//             // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
+//             try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+//         }
+//     };
+//     try std.testing.fuzz(Context{}, Context.testOne, .{});
+// }
