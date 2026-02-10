6299410c6d2cc49e61d912eec3577871c1803c1a
Lucien Faure <faurelucien@proton.me>
2026-02-10 22:39:10 +0100

    stack generic type

--- aoc/src/main.zig
+++ aoc/src/main.zig
@@ -11,44 +11,46 @@ fn print_slice(slice: []const u8) void {
     std.debug.print("\n", .{});
 }
 
+pub fn get_rdn(io: std.Io) std.Random {
+    var seed_as_u8: [8]u8 = undefined;
+    io.random(&seed_as_u8);
+    var prng: std.Random.DefaultPrng = .init(@bitCast(seed_as_u8));
+    const rand = prng.random();
+    return rand;
+}
+
+fn Stack(comptime T: type) type {
+    return struct {
+        separator: T,
+        slice: []T,
+    };
+}
+
+fn get_stack(comptime T: type, separator: T, slice: []T) Stack(T) {
+    var new_stack: Stack(u8) = undefined;
+    new_stack.separator = separator;
+    new_stack.slice = slice;
+    return new_stack;
+}
+
 pub fn main(init: std.process.Init) !void {
+    const lenght = 10;
+
     const gpa = init.gpa;
     const args: []const []const u8 = try init.minimal.args.toSlice(gpa);
     defer gpa.free(args);
 
-    for (1..args.len) |i| {
-        std.debug.print("{s}\n", .{args[i]});
-    }
-    var random: [5]u8 = undefined;
-    for (&random, 0..) |*elem, i| {
+    var random_slice: [lenght]u8 = undefined;
+    for (&random_slice, 0..) |*elem, i| {
         elem.* = @intCast(i);
     }
-    var seed_as_u8: [8]u8 = undefined;
-    init.io.random(&seed_as_u8);
-    var prng: std.Random.DefaultPrng = .init(@bitCast(seed_as_u8));
-    const rand = prng.random();
-    std.Random.shuffle(rand, u8, &random);
-    std.debug.print("random:\n", .{});
-    print_slice(&random);
+    const rnd = get_rdn(init.io);
+    std.Random.shuffle(rnd, u8, &random_slice);
+
+    const my_stack = get_stack(u8, lenght, &random_slice);
+    std.debug.print("stack.slice:\n", .{});
+    print_slice(my_stack.slice);
+    std.debug.print("stack.separator: {}", .{my_stack.separator});
+
     std.debug.print("\n", .{});
 }
-
-// test "simple test" {
-//     const gpa = std.testing.allocator;
-//     var list: std.ArrayList(i32) = .empty;
-
-//     defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-//     try list.append(gpa, 42);
-//     try std.testing.expectEqual(@as(i32, 42), list.pop());
-// }
-
-// test "fuzz example" {
-//     const Context = struct {
-//         fn testOne(context: @This(), input: []const u8) anyerror!void {
-//             _ = context;
-//             // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-//             try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-//         }
-//     };
-//     try std.testing.fuzz(Context{}, Context.testOne, .{});
-// }
